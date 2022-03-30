161af8c4252e79066b10b1a0b4ca507669b26135
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-03-30 19:58:10 -0400

    completed script file. worked with fixing some bugs. Page is working properly.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,56 @@
 // variables for page function //
-var timeDisplayEl = $('#time-display')
+var timeDisplayEl = $("#time-display")
 
 // time display function
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
     timeDisplayEl.text(rightNow);
 }
 
-
 setInterval(displayTime, 1000);
+
+$(document).ready(function() {
+    // save button event listener //
+    $(".saveBtn").on("click", function() {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+})
+
+function timeTracker() {
+    var timeNow = moment().hour();
+    $(".time-block").each(function() {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("furure");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#hour18 .description").val(localStorage.getItem("hour18"));
+
+timeTracker();
